1519800e9e7b0cd84e5ca0eb781c4d1a899896c2
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2025-12-11 14:03:12 +0100

    [Dockerfile] - Modified the configuration
    - Added .gitignore file
    - Added a script to create automatically the CMakelist in each sw component and then
    	launch the build and the tests
    - Pin base image & package versions
    - Improve MISRA/cppcheck scripts with parallel jobs
    - Add CRLF fix
    - Set default CMD to build-and-check-all.sh

---
+++ project/code/UdsComm/docs/html/dir_0f0c216b196b7a3b5b9209212a87618a.js
@@ -0,0 +1,5 @@
+var dir_0f0c216b196b7a3b5b9209212a87618a =
+[
+    [ "diagnostic.h", "diagnostic_8h_source.html", null ],
+    [ "diagnostic_priv.h", "diagnostic__priv_8h_source.html", null ]
+];
---
+++ project/code/UdsComm/docs/html/navtreeindex0.js
@@ -0,0 +1,9 @@
+var NAVTREEINDEX0 =
+{
+"diagnostic_8h_source.html":[0,0,0,0],
+"diagnostic__priv_8h_source.html":[0,0,0,1],
+"dir_0f0c216b196b7a3b5b9209212a87618a.html":[0,0,0],
+"files.html":[0,0],
+"index.html":[],
+"pages.html":[]
+};
